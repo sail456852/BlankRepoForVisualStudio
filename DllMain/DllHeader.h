a74f87ae82b26b31241bc54d53692eec4df23510
wangyuzhen <sail456852@aol.com>
2016-08-16 14:30:27 +0800

    Update DllHeader.h

--- DllMain/DllHeader.h
+++ DllMain/DllHeader.h
@@ -4,7 +4,9 @@
 #define LIBSPY_DLL_H
 
 
-#ifdef DLLMAIN_EXPORTS
+#ifdef DLLMAIN_EXPORTS /*Define the Macro aka ( DLLMAIN_EXPORTS) , in this project's properties settings  (this dll header
+ + responding cpp source file ) ,Properties -> Configuration -> Active(Debug) -> C\C++ -> Preprocessor -> Proprocessor definitions
+*/
 #define LIBSPY_API __declspec(dllexport)
 #else
 #define LIBSPY_API __declspec(dllimport)
